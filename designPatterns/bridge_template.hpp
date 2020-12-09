79d2723bb9f4167d68e3f446d80fec229a7b085b
MrPickle311 <macmacroll@gmail.com>
2020-12-09 23:50:34 +0100

    10.12.2020

---
+++ designPatterns/bridge_template.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+//Podzial logiki biznesowej lub duzej klasy w taki sposob by powstale
+//osobne hierarchie klas pracowaly niezaleznie od siebie
+
+//Jedna z hierarchii to Abstrakcja posiadajaca referencje do obiektu
+//drugiej hierarchii zwanej implementacja
+
+//Szczegolnie przydatne , gdy trzeba wspierac obsluge wielu typow
